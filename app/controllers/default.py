136ce3d80802acc92a4ec42be01d27d5b30d1a00
fernandanlisboa <fernandanlisboa@gmail.com>
2020-08-16 16:44:33 -0300

    definição das rotas

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,15 @@
 from app import app
 #aplicando o método route para a função index 
 #criação de uma página
+@app.route("/index")
 @app.route("/") #rota da minha página index
 def index(): #padrão de chamada da página principal web
     return "Hello World"
+
+@app.route("/test", defaults = {'name': None})
+@app.route("/test<name>")
+def teste(name): #variable default
+    if name:
+      return "Olá %s!" % name
+    else:
+        return "Olá usuário!"
