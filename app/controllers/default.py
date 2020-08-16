828991b5378587967625afec20d6683aa5e6805c
fernandanlisboa <fernandanlisboa@gmail.com>
2020-08-16 17:47:48 -0300

    utilizando templates

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,15 +1,13 @@
+from flask import render_template
 from app import app
 #aplicando o método route para a função index 
 #criação de uma página
-@app.route("/index")
-@app.route("/") #rota da minha página index
-def index(): #padrão de chamada da página principal web
-    return "Hello World"
-
-@app.route("/test", defaults = {'name': None})
-@app.route("/test<name>")
-def teste(name): #variable default
-    if name:
-      return "Olá %s!" % name
-    else:
-        return "Olá usuário!"
+@app.route("/index/<user>" )
+@app.route("/", defaults={"user": None}) #rota da minha página index
+def index(user): #padrão de chamada da página principal web
+    return render_template('base.html',
+                           user = user) #recebe um arquivo html
+    
+@app.route("/login")
+def login():
+      return render_template('base.html')
