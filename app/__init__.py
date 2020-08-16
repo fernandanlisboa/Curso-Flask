2fecc79d8452bac04ab869e8ffaac47cf0e54476
fernandanlisboa <fernandanlisboa@gmail.com>
2020-08-16 15:59:03 -0300

    Adição das tabelas do banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db' #configuração do banco -> uri de conexão do banco de dados
+db = SQLAlchemy(app)
 
 from app.controllers import default
